6c55ed38b658010fc9fdad89130251648b56c023
MatheusFC2 <matheusfreitascampos@gmail.com>
2024-02-23 09:32:07 -0300

    feat: add SideMenu component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,23 +2,33 @@
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import Link from "next/link";
+import SideMenu from "./side-menu";
 import { MenuIcon } from "lucide-react";
-import { signIn, signOut, useSession } from "next-auth/react";
 
 const Header = () => {
-  const { data } = useSession();
-  const handleLogin = async () => {
-    signIn();
-  };
   return (
-    <Card>
-      <CardContent className="p-5 justify-between items-center flex flex-row">
-        <Image src="/logo.png" alt="FSW Barber" height={22} width={120} />
-        <Button variant="outline" size="icon" className="">
-          <MenuIcon size={18} />
-        </Button>
-      </CardContent>
-    </Card>
+    <header>
+      <Card>
+        <CardContent className="p-5 justify-between items-center flex flex-row">
+          <Link href="/">
+            <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+          </Link>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon">
+                <MenuIcon size={16} />
+              </Button>
+            </SheetTrigger>
+
+            <SheetContent className="p-0">
+              <SideMenu />
+            </SheetContent>
+          </Sheet>
+        </CardContent>
+      </Card>
+    </header>
   );
 };
 
